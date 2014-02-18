4c51d1b180dc156cebe729dcacb9337dfd5f37c2
Ken Rufe <ken@halftreelabs.com>
2014-02-18 00:00:48 -0500

    upped the -g to 5 for the 7970

--- scrypt/mine_feathercoins.sh
+++ scrypt/mine_feathercoins.sh
@@ -17,4 +17,4 @@ cd /home/$XUBUNTU/cgminer-2.11.4-x86_64-built
 ./cgminer --scrypt -I 20 -g 1 -w 256 --thread-concurrency 21712 --gpu-engine 1050 --gpu-memclock 1400 --gpu-vddc 1.087 --temp-target 80 --auto-fan -o stratum+tcp://ftc.give-me-coins.com:3336 -u $MINERU1 -p $MINERP1 --failover-only -o stratum+tcp://coinotron.com:3337 -u $MINERU2 -p $MINERP2
 
 #THIS IS 7970
-./cgminer --scrypt -I 13 -g 2 -w 256 --thread-concurrency 8192 --gpu-engine 1140 --gpu-memclock 1500 --gpu-powertune 20 --gpu-vddc 1.170 --temp-target 60 --auto-fan -o stratum+tcp://ftc.give-me-coins.com:3336 -u $MINERU1 -p $MINERP1 --failover-only -o stratum+tcp://coinotron.com:3337 -u $MINERU2 -p $MINERP2
+./cgminer --scrypt -I 13 -g 5 -w 256 --thread-concurrency 8192 --gpu-engine 1140 --gpu-memclock 1500 --gpu-powertune 20 --gpu-vddc 1.170 --temp-target 60 --auto-fan -o stratum+tcp://ftc.give-me-coins.com:3336 -u $MINERU1 -p $MINERP1 --failover-only -o stratum+tcp://coinotron.com:3337 -u $MINERU2 -p $MINERP2
